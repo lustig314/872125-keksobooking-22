9f03aaa71b23d6cc4826171841157000ef6674ba
lustig314 <60037706+lustig314@users.noreply.github.com>
2021-02-06 09:04:34 +0300

    Добавил функцию, возвращающую случайный элемент массива и объект типа дома

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
-const getRandomInteger = (min, max) => {
+const getRandomInteger = (min = 0, max = 10000) => {
   if (min < 0 || max < 0 || min > max || min === max) {
     return -1;
   }
@@ -18,7 +18,28 @@ const getRandomFractional = (min, max, afterComma) => {
   return Number(randomNumber.toFixed(afterComma));
 };
 
+// Функция, возвращающая случайный элемент массива
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInteger(0, elements.length-1)];
+};
+
+const TYPES_HOUSES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+
 const author = {
   avatar: 'img/avatars/user0' + getRandomInteger(1, 8) + '.png',
 };
+
+const offer = {
+  title: 'Только сегодня!',
+  adress: 'location.x + location.y',
+  price: getRandomInteger(),
+  type: getRandomArrayElement(TYPES_HOUSES),
+}
+
 
