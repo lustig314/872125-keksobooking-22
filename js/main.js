8d8fe0a18011a54a18da280eb9a12ebc51de1c0d
lustig314 <60037706+lustig314@users.noreply.github.com>
2021-02-10 01:55:13 +0300

    Дописал основную часть, осталось соединить объекты

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const getRandomInteger = (min = 0, max = 10000) => {
 };
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-const getRandomFractional = (min, max, afterComma) => {
+const getRandomFractional = (min, max, afterComma = 5) => {
   if (min < 0 || max < 0 || min > max || min === max) {
     return -1;
   }
@@ -20,21 +20,29 @@ const getRandomFractional = (min, max, afterComma) => {
 
 // Функция, возвращающая случайный элемент массива
 const getRandomArrayElement = (elements) => {
-  return elements[getRandomInteger(0, elements.length-1)];
+  return elements[getRandomInteger(0, elements.length - 1)];
 };
 
-//Функция, возвращающая массив случайной длины из определенных значений
-
+//Функция, возвращающая массив случайной длины из случайных, неповторяющихся значений.
 const getRandomArrayLength = (array) => {
-  return
+  const arrayRandomLength = [];
+  let randomVote;
+  array.forEach((value) => {
+    randomVote = getRandomInteger(0, 1);
+    if (randomVote === 1) {
+      arrayRandomLength.push(value);
+    }
+  });
+  if (arrayRandomLength.length === 0) {
+    arrayRandomLength.push(array[getRandomInteger(0, array.length - 1)])
+  }
+  return arrayRandomLength;
 };
-
-
 
 const OFFER_HEADERS = [
-  'Только сегодня!',
-  'Акция!!!',
-  'Лучшее предложение!',
+  'Only today!',
+  'Sale!!!',
+  'Best offer!',
 
 ];
 const TYPES_HOUSES = [
@@ -56,21 +64,42 @@ const LIST_FEATURES = [
   'washer',
   'elevator',
   'conditioner',
+];
 
-]
+const PHOTOS_HOUSES = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+const SIMILAR_ADS_COUNT = 10;
 
 const author = {
   avatar: 'img/avatars/user0' + getRandomInteger(1, 8) + '.png',
 };
 
+const location = {
+  x: getRandomFractional(35.65000, 35.70000),
+  y: getRandomFractional(139.70000, 139.80000),
+};
+
 const offer = {
   title: getRandomArrayElement(OFFER_HEADERS),
-  adress: 'location.x + location.y',
-  price: getRandomInteger(),
+  adress: location.x + ', ' + location.y,
   type: getRandomArrayElement(TYPES_HOUSES),
-  rooms: getRandomInteger(),
-  guests: getRandomInteger(),
+  rooms: getRandomInteger(1, 15),
+  guests: getRandomInteger(2, 30),
   checkin: getRandomArrayElement(CHECK_TIMES),
   checkout: getRandomArrayElement(CHECK_TIMES),
-  feauters:
+  feauters: getRandomArrayLength(LIST_FEATURES),
+  description: getRandomArrayElement(OFFER_HEADERS) + ' Comfortable ' + getRandomArrayElement(TYPES_HOUSES) + ' with ' + getRandomInteger(1, 15) + ' rooms, which can accommodate ' + getRandomInteger(2, 30) + ' guests' + '. At an attractive price: ' + getRandomInteger(),
+  photos: getRandomArrayLength(PHOTOS_HOUSES),
 };
+
+const createAds = () => {
+  return author + offer + location;
+};
+
+const similarAdsNearby = new Array(SIMILAR_ADS_COUNT).fill(null).map(() => createAds());
+
+console.log(similarAdsNearby);
